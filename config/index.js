b3143b75a7f6fd8b880e758fbfb8df1217d0d507
andry-baka <andry.baka@mitrais.com>
2017-02-13 00:38:00 +0700

    Change port

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  port: 80 || process.env.PORT,     //  What port does our server run on?
+  port: 3000 || process.env.PORT,     //  What port does our server run on?
   documentationPath: '/documentation' //  What is the path to the docs?
 };
