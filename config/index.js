1ef1b9efb6de9e95787102dc925a091689276e8d
andry-baka <andry.baka@mitrais.com>
2017-02-13 00:53:24 +0700

    Change port syntax

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  port: 3000 || process.env.PORT,     //  What port does our server run on?
+  port: process.env.PORT || 8080,     //  What port does our server run on?
   documentationPath: '/documentation' //  What is the path to the docs?
 };
